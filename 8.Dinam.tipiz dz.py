3ccf1b4d2e7f7dd798812fae63da824226713d25
an94york <an.chernuxa@yandex.ru>
2024-08-15 10:27:05 +0300

    8.Dinam.tipiz dz.py

--- 8.Dinam.tipiz dz.py
+++ 8.Dinam.tipiz dz.py
@@ -6,3 +6,4 @@ new_age = 35
 print(new_age)
 is_student = (True)
 print(is_student)
+print(f'Name: {name}, Age: {age}, New age: {new_age}, Is student: {is_student}') # не поняла как переносить на новую строку, стобы каждая переменная была с новой строки в столббик а не в строчку
